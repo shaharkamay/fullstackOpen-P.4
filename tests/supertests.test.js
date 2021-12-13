866415399f442dd0428139a7bc3be7c441133c26
shaharKamay <shahar.kamay@gmail.com>
2021-12-13 21:29:37 +0200

    done 4.10

--- tests/supertests.test.js
+++ tests/supertests.test.js
@@ -12,7 +12,7 @@ test("blogs are returned as json", async () => {
     .expect(200)
     .expect("Content-Type", /application\/json/)
 
-  expect(result.body.length).toBe(6);
+  expect(result.body.length).toBe(11);
 });
 
 test("verify that the unique identifier property of the blog posts is named id", async () => {
@@ -22,6 +22,23 @@ test("verify that the unique identifier property of the blog posts is named id",
     .expect("Content-Type", /application\/json/)
 
   expect(result.body[0].id).toBeDefined();
+})
+
+test("verify that a blog added to DB", async () => {
+  const blogs1 = await api
+    .get("/api/blogs")
+
+  await api.post("/api/blogs").send({
+    "title": "test",
+    "author": "test",
+    "url": "test",
+    "likes": 0,
+  }).expect(200)
+
+  const blogs2 = await api
+    .get("/api/blogs")
+
+  expect(blogs1.body.length + 1).toBe(blogs2.body.length);
 })
 
 afterAll(() => {
